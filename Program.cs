d2432b604283a3473f36a7f5c8bed708521aff4b
Samaneh Golanvaz <samanehgolnavaz@gmail.com>
2023-09-22 13:17:30 +0330

    Add UserStoreDefault Add EFCore

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 using Identity_Samples;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var connectionString = "Data Source=LAPTOP-VAHA5KRU\\sql_2022;Initial Catalog=Identity_Samples2;Persist Security Info=True;TrustServerCertificate=True;User ID=sa;Password=sql";
+var migrationAssembly=typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+builder.Services.AddDbContext<IdentityDbContext>(
+    opt => opt.UseSqlServer(connectionString,sql =>sql.MigrationsAssembly(migrationAssembly))) ;
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddIdentityCore<User>(options => { });
-builder.Services.AddScoped<IUserStore<User>,UserStore>();
+builder.Services.AddIdentityCore<IdentityUser>(options => { });
+builder.Services.AddScoped<IUserStore<IdentityUser>,
+    UserOnlyStore<IdentityUser,IdentityDbContext>>();
 builder.Services.AddAuthentication("cookies").AddCookie(options => options.LoginPath="/Home/Login");
 var app = builder.Build();
 
